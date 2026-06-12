1ebbb636e20245110c6cbd7befd3234a1220e0c5
unish615 <unishgautam07@gmail.com>
2026-06-12 21:48:26 +0545

    buttons fixed

--- src/view/Sender_profile.java
+++ src/view/Sender_profile.java
@@ -238,7 +238,7 @@ public class Sender_profile extends javax.swing.JFrame {
                     .addGroup(jPanel3Layout.createSequentialGroup()
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(resetpasswordButton)
-                        .addGap(48, 48, 48)
+                        .addGap(30, 30, 30)
                         .addComponent(saveButton))
                     .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel3Layout.createSequentialGroup()
                         .addGap(160, 160, 160)

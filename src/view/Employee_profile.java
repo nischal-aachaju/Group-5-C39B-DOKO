9c63b675b9d178236effa86d171aa01bd399b9ec
unish615 <unishgautam07@gmail.com>
2026-06-12 21:30:53 +0545

    final ui

--- src/view/Employee_profile.java
+++ src/view/Employee_profile.java
@@ -220,7 +220,7 @@ public class Employee_profile extends javax.swing.JFrame {
                     .addGroup(jPanel3Layout.createSequentialGroup()
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(resetpasswordButton)
-                        .addGap(48, 48, 48)
+                        .addGap(88, 88, 88)
                         .addComponent(saveButton))
                     .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel3Layout.createSequentialGroup()
                         .addGap(160, 160, 160)
